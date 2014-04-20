dec0fe14ba4eb037299b6fb74dcd8d3d5c469fab
thakyuu <x.rozenmaiden@gmail.com>
2014-04-21 07:18:30 +0900

    First Commit

---
+++ Windows_mklink_vim.bat
@@ -0,0 +1,3 @@
+mklink "C:\Users\x\_vimrc" D:\Dropbox\dotfiles\_vimrc
+mklink "C:\Program Files\vim\vimrc" D:\Dropbox\dotfiles\system\vimrc
+mklink "C:\Program Files\vim\gvimrc" D:\Dropbox\dotfiles\system\gvimrc

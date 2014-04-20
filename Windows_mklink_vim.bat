2b72afa2cdcc99e411c4fad13bc754f47cece5f4
thakyuu <x.rozenmaiden@gmail.com>
2014-04-21 07:20:29 +0900

    Fix mklink

--- Windows_mklink_vim.bat
+++ Windows_mklink_vim.bat
@@ -1,3 +1,3 @@
-mklink "C:\Users\x\_vimrc" D:\Dropbox\dotfiles\_vimrc
+mklink "C:\Users\x\_vimrc" D:\Dropbox\dotfiles\user\_vimrc
 mklink "C:\Program Files\vim\vimrc" D:\Dropbox\dotfiles\system\vimrc
 mklink "C:\Program Files\vim\gvimrc" D:\Dropbox\dotfiles\system\gvimrc
